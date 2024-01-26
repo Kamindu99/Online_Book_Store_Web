a7a8786c9f686fc145c8beac9ca006ea2f9f331f
kamindu.g <kamindu.g@informaticsint.com>
2024-01-26 14:27:06 +0530

    feat:screen desc:: dash color

--- src/pages/home/dashboard.tsx
+++ src/pages/home/dashboard.tsx
@@ -1,30 +1,47 @@
 // material-ui
-
 import { Grid } from "@mui/material";
-import AnalyticEcommerce from "components/cards/statistics/AnalyticBooks";
+import { useTheme } from '@mui/material/styles';
+import EcommerceMetrix from "components/cards/statistics/EcommerceMetrix";
 
 // project import
+import {
+    ScheduleFilled
+} from '@ant-design/icons';
 
 // ==============================|| Dashboard ||============================== //
 
-const Dashboard = () => (
-    <>
-        <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <AnalyticEcommerce title="Total Books" count="100" />
+const Dashboard = () => {
+    const theme = useTheme();
+    return (
+        <>
+            <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+                <Grid item xs={12} lg={4} sm={6}>
+                    <EcommerceMetrix
+                        primary="All Books"
+                        secondary="100"
+                        color={theme.palette.primary.main}
+                        iconPrimary={ScheduleFilled}
+                    />
+                </Grid>
+                <Grid item xs={12} lg={4} sm={6}>
+                    <EcommerceMetrix
+                        primary="Disposed Books"
+                        secondary="80"
+                        color={theme.palette.warning.main}
+                        iconPrimary={ScheduleFilled}
+                    />
+                </Grid>
+                <Grid item xs={12} lg={4} sm={12}>
+                    <EcommerceMetrix
+                        primary="Available Books"
+                        secondary="30"
+                        color={theme.palette.success.main}
+                        iconPrimary={ScheduleFilled}
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <AnalyticEcommerce title="Total Disposal" count="80" />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <AnalyticEcommerce title="Total Missing" count="20" isLoss color="warning" />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <AnalyticEcommerce title="Total Read" count="30" isLoss color="warning" />
-            </Grid>
-            <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
-        </Grid>
-    </>
-);
+        </>
+    );
+}
 
 export default Dashboard;
