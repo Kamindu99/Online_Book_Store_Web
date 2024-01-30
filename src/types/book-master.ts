9300824d214aef3d3fc0616a5acbe383b55a90e0
kamindu.g <kamindu.g@informaticsint.com>
2024-01-31 01:55:34 +0530

    fix:update desc:: fix

--- src/types/book-master.ts
+++ src/types/book-master.ts
@@ -11,7 +11,7 @@ export interface BookStateProps {
     books: Books[] | null;
     error: object | string | null;
     success: object | string | null;
-
+    isLoading: boolean
 }
 
 export interface DefaultRootStateProps {
