980d7f5e8b9d4b484f725ad298999e267b36cdfa
kamindu.g <kamindu.g@informaticsint.com>
2024-02-01 00:35:36 +0530

    fix:update desc:: fix

--- src/types/user-profile.ts
+++ src/types/user-profile.ts
@@ -4,23 +4,7 @@ export type UserProfile = {
   image?: string;
   name?: string;
   role?: string;
-  about?: string;
-  email?: string;
-  work_email?: string;
-  personal_email?: string;
-  phone?: string;
-  work_phone?: string;
-  personal_phone?: string;
-  birthdayText?: string;
-  lastMessage?: string;
-  status?: string;
-  friends?: number;
-  followers?: number;
-  contact?: string;
-  company?: string;
-  location?: string;
-  online_status?: string;
-  unReadChatCount?: number;
+
   // groups?: Group[];
   time?: string;
   tier?: string;
