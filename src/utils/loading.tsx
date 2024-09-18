319ade9a316633453108bc4c8c0e2057ac869bb9
kamindu.g <kamidugayantha123@gmail.com>
2024-09-18 21:19:39 +0530

    fix:update desc:: user password update

--- src/utils/loading.tsx
+++ src/utils/loading.tsx
@@ -1,15 +1,16 @@
+import { Grid } from '@mui/material'
 import './list.css'
 
 export function Loading() {
     return (
         <>
-            <div style={{ margin: "10%" }}>
-                <div style={{ display: "flex", alignContent: "center", justifyContent: "center" }}>
+            <Grid container spacing={2} >
+                <div className='loading-container' style={{ display: "flex", alignContent: "center", justifyContent: "center" }}>
 
                     <img src={'https://i.gifer.com/Ybin.gif'} alt="Loading..." className="zoom-logo" />
 
                 </div>
-            </div>
+            </Grid>
         </>
     )
 }
