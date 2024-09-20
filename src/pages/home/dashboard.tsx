07cb34746e5e78dfda21195444a12109855b9fd3
kamindu.g <kamidugayantha123@gmail.com>
2024-09-20 16:57:00 +0530

    fix:update desc:: dashboard

--- src/pages/home/dashboard.tsx
+++ src/pages/home/dashboard.tsx
@@ -1,18 +1,14 @@
 // material-ui
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
-import EcommerceMetrix from "components/cards/statistics/EcommerceMetrix";
 
 // project import
-import {
-    ScheduleFilled
-} from '@ant-design/icons';
+import AnalyticsDataCard from "components/cards/statistics/AnalyticsDataCard";
+import MainCard from "components/MainCard";
 import { useEffect } from "react";
+import InquiryStatusPieChart from "sections/dashboard/dashboard/InquiryStatusPieChart";
 import { useDispatch, useSelector } from "store";
 import { getBooksCount } from "store/reducers/book-master";
-import MainCard from "components/MainCard";
-import InquiryStatusPieChart from "sections/dashboard/dashboard/InquiryStatusPieChart";
-import AnalyticsDataCard from "components/cards/statistics/AnalyticsDataCard";
 
 // ==============================|| Dashboard ||============================== //
 
@@ -29,42 +25,52 @@ const Dashboard = () => {
     return (
         <>
             <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Grid item xs={6} sm={6} md={4} lg={4}>
                     <AnalyticsDataCard title="Total Users" count={booksCount?.totalUsers?.toString()!} children={undefined} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Grid item xs={6} sm={6} md={4} lg={4}>
                     <AnalyticsDataCard title="Total Books" count={booksCount?.totalBooks?.toString()!} isLoss color="warning" children={undefined} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
+                <Grid item xs={12} sm={6} md={4} lg={4}>
                     <AnalyticsDataCard title="Total Borrow" count={booksCount?.totalTransfers?.toString()!} isLoss color="warning" children={undefined} />
                 </Grid>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                    <AnalyticsDataCard title="Total Loans" count="5" percentage={70.5} children={undefined} />
-                </Grid>
 
-                <Grid item xs={7} md={7} lg={7}>
-                    <Grid item xs={12} lg={12} sm={12}>
-                        <EcommerceMetrix
-                            primary="All Books"
-                            secondary={booksCount?.totalBooks?.toString()}
-                            color={theme.palette.secondary.main}
-                            iconPrimary={ScheduleFilled}
-                            size="large"
-                        />
+                <Grid item xs={12} md={7} lg={7}>
+                    <Grid container alignItems="center" justifyContent="space-between">
+                        <Grid item>
+                            <Typography variant="h5">Category Wise Books</Typography>
+                        </Grid>
                     </Grid>
-                    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-                        {booksCount?.category?.map((item, index) => (
-                            <Grid item xs={4} lg={4} sm={4}>
-                                <EcommerceMetrix
-                                    primary={item.categoryName}
-                                    secondary={item.count?.toString()}
-                                    color={theme.palette.primary.darker}
-                                />
-                            </Grid>
-                        ))}
-                    </Grid>
+                    <MainCard content={false} sx={{ mt: 2 }}>
+                        <Grid item xs={12} lg={12} sm={12} >
+                            <Card sx={{ background: theme.palette.primary.lighter, borderRadius: 0 }}>
+                                <CardContent>
+                                    <Typography variant="h4" component="h2">
+                                        Total Books - {booksCount?.totalBooks} Books
+                                    </Typography>
+
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid container rowSpacing={4.5} columnSpacing={1.5} p={1.5}>
+                            {booksCount?.category?.map((item, index) => (
+                                <Grid item xs={6} lg={3} sm={3}>
+                                    <Card sx={{ background: '#162d78' }}>
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2" color={'white'}>
+                                                {item.categoryName}
+                                            </Typography>
+                                            <Typography variant="body1" color="#b5b5b5" component="p">
+                                                {item.count} Books
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </MainCard>
                 </Grid>
-                <Grid item xs={5} md={5} lg={5}>
+                <Grid item xs={12} md={5} lg={5}>
                     <Grid container alignItems="center" justifyContent="space-between">
                         <Grid item>
                             <Typography variant="h5">Books Overview</Typography>
