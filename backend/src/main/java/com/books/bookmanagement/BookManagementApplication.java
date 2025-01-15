d37bd6ed856e673eae31f8bf6a1213b0f21c82ef
kamindu.g <kamidugayantha123@gmail.com>
2025-01-15 23:26:49 +0530

    fix backend

--- backend/src/main/java/com/books/bookmanagement/BookManagementApplication.java
+++ backend/src/main/java/com/books/bookmanagement/BookManagementApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookManagementApplication.class, args);
 	}
