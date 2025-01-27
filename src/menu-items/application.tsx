1f139bd31f0e09278244517368da56f118a01016
kamindu.g <kamidugayantha123@gmail.com>
2025-01-28 01:05:38 +0530

    change icons

--- src/menu-items/application.tsx
+++ src/menu-items/application.tsx
@@ -6,15 +6,13 @@ import { FormattedMessage } from 'react-intl';
 // type
 import { NavItemType } from 'types/menu';
 
-// icons
-
 // ==============================|| MENU ITEMS - SUPPORT ||============================== //
 
 const application: NavItemType = {
   id: 'application',
   title: <FormattedMessage id="application" />,
   type: 'group',
-  children: [ 
+  children: [
   ]
 };
 
