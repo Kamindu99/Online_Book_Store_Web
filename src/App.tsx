9bf9ff970390725f0b7082f1dfc41e3b200c03c3
kamindu.g <kamidugayantha123@gmail.com>
2025-02-11 01:47:57 +0530

    app file chage

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,6 @@ import { fetchMenu } from 'store/reducers/menu';
 
 // auth-provider
 import { JWTProvider as AuthProvider } from 'contexts/JWTContext';
-// import { FirebaseProvider as AuthProvider } from 'contexts/FirebaseContext';
-// import { AWSCognitoProvider as AuthProvider } from 'contexts/AWSCognitoContext';
-// import { Auth0Provider as AuthProvider } from 'contexts/Auth0Context';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
